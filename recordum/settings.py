769708bd4576f19d8f739c551d3d5621d0a7984f
Daniely Alves <thales_ximenes@hotmail.com>
2022-07-05 19:00:09 -0300

    Ultimas modificacoes para deploy

--- recordum/settings.py
+++ recordum/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-cl=$h%uvrso#=v-y)6ussue@g9w*_b^^5*w!jts4!&@4oa4y5i'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-cl=$h%uvrso#=v-y)6ussue@g9w*_b^^5*w!jts4!&@4oa4y5i'
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
