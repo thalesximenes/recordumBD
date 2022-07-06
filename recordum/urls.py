4e88eaf7474c5f5ea2ce3da27e2c734315df9106
Thales Fued Ximenes Ferreira <e80298510@mpce.mp.br>
2022-07-06 08:39:51 -0300

    adicionando diretorio da pasta media

--- recordum/urls.py
+++ recordum/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     path('admin/', admin.site.urls),
     path('auth/', include('usuarios.urls')),
     path('conteudo/', include("disciplinas.urls", namespace='disciplinas')),
